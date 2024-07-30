5cb84710252860ffb3261eb82aab88e3cb3b8fa0
GV-Hariharan <154255232+GV-Hariharan@users.noreply.github.com>
2024-07-30 13:20:50 +0530

    Update 30-7-24.java

--- 30-7-24.java
+++ 30-7-24.java
@@ -26,4 +26,53 @@ class StringBufferExample{
     }
 }
 
-4)
+4)DELETE
+class StringBufferExample{
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer("Hello ");
+        sb.delete(1,3);
+        System.out.println(sb);
+    }
+}
+
+5)REVERSE
+class StringBufferExample{
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer("Hello ");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
+
+6)CAPACITY
+public class newstringbuffer {
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer();
+        System.out.println(sb.capacity());
+        sb.append("hello");
+        System.out.println(sb.capacity());
+        sb.append("java is my favourite ");
+        System.out.println(sb.capacity());
+        
+    }
+    
+}
+
+7)ENSURE
+public class newstringbuffer {
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer();
+        System.out.println(sb.capacity());
+        sb.append("hello");
+        System.out.println(sb.capacity());
+        sb.append("java is my favourite ");
+        System.out.println(sb.capacity());
+        sb.ensureCapacity(10);
+        System.out.println(sb.capacity());
+        sb.ensureCapacity(50);
+        System.out.println(sb.capacity());
+    }
+    
+}
+
+
